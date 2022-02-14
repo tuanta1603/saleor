edc256f9d9557f5a2124dc354d396ce0aaad9eda
IKarbowiak <iga.karbowiak@mirumee.com>
2022-02-14 11:01:18 +0100

    Fix failing on_failure export tasks method

--- saleor/csv/tasks.py
+++ saleor/csv/tasks.py
@@ -1,5 +1,7 @@
 from typing import Dict, Union
 
+import celery
+
 from ..celeryconf import app
 from ..core import JobStatus
 from . import events
@@ -8,37 +10,44 @@ from .notifications import send_export_failed_info
 from .utils.export import export_gift_cards, export_products
 
 
-def on_task_failure(self, exc, task_id, args, kwargs, einfo):
-    export_file_id = args[0]
-    export_file = ExportFile.objects.get(pk=export_file_id)
+class ExportTask(celery.Task):
+    # should be updated when new export task is added
+    TASK_NAME_TO_DATA_TYPE_MAPPING = {
+        "export-products": "products",
+        "export-gift-cards": "gift cards",
+    }
 
-    export_file.content_file = None
-    export_file.status = JobStatus.FAILED
-    export_file.save(update_fields=["status", "updated_at", "content_file"])
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        export_file_id = args[0]
+        export_file = ExportFile.objects.get(pk=export_file_id)
 
-    events.export_failed_event(
-        export_file=export_file,
-        user=export_file.user,
-        app=export_file.app,
-        message=str(exc),
-        error_type=str(einfo.type),
-    )
+        export_file.content_file = None
+        export_file.status = JobStatus.FAILED
+        export_file.save(update_fields=["status", "updated_at", "content_file"])
 
-    send_export_failed_info(export_file)
+        events.export_failed_event(
+            export_file=export_file,
+            user=export_file.user,
+            app=export_file.app,
+            message=str(exc),
+            error_type=str(einfo.type),
+        )
+
+        data_type = ExportTask.TASK_NAME_TO_DATA_TYPE_MAPPING.get(self.name)
+        send_export_failed_info(export_file, data_type)
+
+    def on_success(self, retval, task_id, args, kwargs):
+        export_file_id = args[0]
+
+        export_file = ExportFile.objects.get(pk=export_file_id)
+        export_file.status = JobStatus.SUCCESS
+        export_file.save(update_fields=["status", "updated_at"])
+        events.export_success_event(
+            export_file=export_file, user=export_file.user, app=export_file.app
+        )
 
 
-def on_task_success(self, retval, task_id, args, kwargs):
-    export_file_id = args[0]
-
-    export_file = ExportFile.objects.get(pk=export_file_id)
-    export_file.status = JobStatus.SUCCESS
-    export_file.save(update_fields=["status", "updated_at"])
-    events.export_success_event(
-        export_file=export_file, user=export_file.user, app=export_file.app
-    )
-
-
-@app.task(on_success=on_task_success, on_failure=on_task_failure)
+@app.task(name="export-products", base=ExportTask)
 def export_products_task(
     export_file_id: int,
     scope: Dict[str, Union[str, dict]],
@@ -50,7 +59,7 @@ def export_products_task(
     export_products(export_file, scope, export_info, file_type, delimiter)
 
 
-@app.task(on_success=on_task_success, on_failure=on_task_failure)
+@app.task(name="export-gift-cards", base=ExportTask)
 def export_gift_cards_task(
     export_file_id: int,
     scope: Dict[str, Union[str, dict]],
